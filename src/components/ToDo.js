3671f584ab34d29cf8d9b688e8848bed90e729a9
leshista <a.leshista@gmail.com>
2022-08-18 18:13:09 +0300

    added todochanging structure

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,10 +6,6 @@ const ToDo = () => {
     // I split the whole container into two blocks
     return (
         <main className={styles.toDo}>
-            {/* And to the right the user
-            should be able to change tasks'
-            content, mark them as 'waiting',
-            'in process' and 'done'*/}
             <ToDoList />
             <ToDoChanging />
         </main>
--- src/components/ToDoChanging.js
+++ src/components/ToDoChanging.js
@@ -1,8 +1,18 @@
 import styles from '../componentStyles/css/ToDoChanging.module.css';
 const ToDoChanging = () => {
+    // In the right the user should
+    //  be able to change tasks'
+    // content, mark them as
+    // 'waiting', 'in process' and 'done'
     return (
         <section className={styles.toDoChanging}>
-            <div></div>
+            <h2 className={styles.toDoChanging__Title}>Lorem Ipsum</h2>
+            <p className={styles.toDoChanging__Text}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident corrupti architecto saepe aperiam numquam nemo iusto
+                repellendus excepturi repellat eligendi odit in cum fuga qui
+                nobis, perspiciatis esse odio magnam.
+            </p>
         </section>
     );
 };
