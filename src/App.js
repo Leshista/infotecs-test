5e86fca2e85f09e415a697fdd1958b32c0835ead
leshista <a.leshista@gmail.com>
2022-08-18 14:14:55 +0300

    created basic ToDo.js structure, created ToDoItem.js, commented a bit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import styles from './App.module.css';
+import styles from './App.module.css'; // I find styles modules the best to use within React apps, so use them in all components
 import ToDo from './components/ToDo';
 
 const App = () => {
+    // The main purpose of this component is to me container of all that is ToDo
+    // And to be rendered into html
     return (
         <>
             <header className={styles.header}>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,3 +1,23 @@
 import styles from '../componentStyles/css/ToDo.module.css';
-const ToDo = () => {};
+import ToDoListItem from './ToDoListItem';
+const ToDo = () => {
+    <main className={styles.toDoContainer}>
+        {/* I split the whole container into two blocks:
+            To the left the user should be able to choose
+            tasks, add and delete them */}
+        <aside className={styles.toDoList}>
+            <ToDoListItem />
+            <ToDoListItem />
+            <ToDoListItem />
+            <ToDoListItem />
+            <ToDoListItem />
+        </aside>
+        {/* And to the right the user
+            should be able to change tasks'
+            content, mark them as 'waiting',
+            'in process' and 'done'*/}
+        <main className={styles.toDoChanging}></main>
+    </main>;
+};
+
 export default ToDo;
