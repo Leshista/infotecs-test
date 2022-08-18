4f82c661d3f530968513b7b9ec36b5d2f2b6db74
leshista <a.leshista@gmail.com>
2022-08-18 18:42:01 +0300

    Now we can add tasks

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import styles from '../componentStyles/css/ToDoList.module.css';
 import ToDoListItem from './ToDoListItem';
 
 const ToDoList = () => {
     // In the left block the user should be
     // able to choose tasks, add and delete them
+
+    // States
+    const [todos, setTodos] = useState([]);
+
+    // Handlers
+    const addTodoHandler = () => {
+        setTodos([...todos, {}]);
+    };
+    // const deleteTodoHandler = () => setTodos(filter(todos));
+
     return (
         <aside className={styles.toDoList}>
             <input
@@ -12,13 +23,19 @@ const ToDoList = () => {
                 id=""
                 className={styles.toDoList__Search}
             />
-            <ToDoListItem />
-            <ToDoListItem />
-            <ToDoListItem />
-            <ToDoListItem />
-            <ToDoListItem />
+            {todos.map((todo) => (
+                <ToDoListItem
+                    id={todos.indexOf(todo)}
+                    key={todos.indexOf(todo)}
+                />
+            ))}
             <div className={styles.toDoList__Buttons}>
-                <button className={styles.toDoList__Add}>Add</button>
+                <button
+                    className={styles.toDoList__Add}
+                    onClick={addTodoHandler}
+                >
+                    Add
+                </button>
                 <button className={styles.toDoList__Delete}>Delete</button>
             </div>
         </aside>
--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -3,7 +3,7 @@ import styles from '../componentStyles/css/ToDoListItem.module.css';
 const ToDoListItem = () => {
     return (
         <article className={styles.toDoList__Item}>
-            <p className={styles.toDoList__text}>Lorem ipsum</p>
+            <p className={styles.toDoList__text}>New Task</p>
         </article>
     );
 };
